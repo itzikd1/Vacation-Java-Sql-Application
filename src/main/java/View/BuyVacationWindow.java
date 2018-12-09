bbf14472f0b1154a368b3f4b9d45caf98ac9c5bc
itzik <itzikd15@gmail.com>
2018-12-10 01:29:27 +0200

    Merge branch 'master' of https://github.com/itzikd1/IRMAFD
    # Conflicts:
    #	Vaction4U.sqlite
    #	src/main/java/View/BuyVacationWindow.java
    #	src/main/java/View/RequestForBuyerColumn.java

--- src/main/java/View/BuyVacationWindow.java
+++ src/main/java/View/BuyVacationWindow.java
@@ -1,23 +1,37 @@
 package View;
 
+import Controller.Controller;
+import javafx.embed.swing.JFXPanel;
 import javafx.event.ActionEvent;
 import javafx.fxml.Initializable;
+import javafx.stage.Stage;
 
+import java.awt.*;
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class BuyVacationWindow implements Initializable {
-
+    public Button ToggleButton;
+    public Button BackButton;
+    private Controller controller = Controller.getInstance();
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
-        //hello this is my code
-        //you should take it
     }
 
+
     public void go_main(ActionEvent actionEvent) {
+        // TODO: 10/12/2018  doesnt not work for some reason  Itzik
+//        Stage s = (Stage) BackButton.getScene().getWindow();
+//        s.close();
     }
 
     public void pay(ActionEvent actionEvent) {
+    }
+
+    public void toggleChange(ActionEvent actionEvent) {
+        // TODO: 10/12/2018 if this works (wait for maor to finshe connection) then here we chose
+        // TODO: between paypal and regular payment, button diable...enable..  Itzik
+        ToggleButton.addActionListener(e -> System.out.println("hello"));
     }
 }
